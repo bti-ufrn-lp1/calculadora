027cb0337d39be84822ef9f42218f5b22d832066
Everton Cavalcante <evertonranielly@gmail.com>
2022-04-04 22:52:40 -0300

    Implementação inicial de arquivo cabeçalho com protótipos de funções

---
+++ calculadora.h
@@ -0,0 +1,14 @@
+#ifndef CALCULADORA_H
+#define CALCULADORA_H
+
+void imprimir(float x);
+
+int somar(int x, int y);
+
+int subtrair(int x, int y);
+
+int multiplicar(int x, int y);
+
+float dividir(int x, int y);
+
+#endif
